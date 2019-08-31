99e65fdf12dae17564819926382ddbdf7ce4de4a
Jacky <liangj97@gmail.com>
2019-08-31 17:54:57 +1000

    Add hammerjs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { environment } from './environments/environment';
+import 'hammerjs';
 
 import { AppModule } from './app/app.module';
 
